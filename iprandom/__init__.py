bdd29115fda44ab8280656401d61d09582d5395b
jackwardell <jackwardell@me.com>
2020-03-11 10:49:07 +0000

    finished software ips

--- iprandom/__init__.py
+++ iprandom/__init__.py
@@ -18,17 +18,27 @@ special_address_blocks = {
 # 192.168.0.0/16  =  192.168.0.0–192.168.255.255
 # 198.18.0.0/15   =  198.18.0.0–198.19.255.255
 
+# software ip cidr
+# 0.0.0.0/8	= 0.0.0.0–0.255.255.255
 
-def ipv4_address(private_network_allowed: bool = True):
+# host ip cidr
+# 127.0.0.0/8 = 127.0.0.0–127.255.255.255
+
+
+def ipv4_address(
+        private_network_allowed: bool = True,
+        software_allowed: bool = True,
+        host_allowed: bool = True,
+):
     """
     get a random ip address
     """
     ip_address_parts = [random.randint(0, 255) for _ in range(4)]
 
-    if private_network_allowed:
+    def _construct_ip_address():
         return ".".join([str(part) for part in ip_address_parts])
 
-    elif not private_network_allowed:
+    if not private_network_allowed:
         # 10.0.0.0/8 = 10.0.0.0–10.255.255.255
         if ip_address_parts[0] == 10:
             return ipv4_address(private_network_allowed=False)
@@ -49,8 +59,23 @@ def ipv4_address(private_network_allowed: bool = True):
         elif ip_address_parts[0:2] == [192, 168]:
             return ipv4_address(private_network_allowed=False)
 
+        # 198.18.0.0/15 = 198.18.0.0–198.19.255.255
+        elif ip_address_parts[0:2] in ([198, 18], [198, 19]):
+            return ipv4_address(private_network_allowed=False)
+
         else:
-            return ".".join([str(part) for part in ip_address_parts])
+            return _construct_ip_address()
+
+    if not software_allowed:
+        # 0.0.0.0/8	= 0.0.0.0–0.255.255.255
+        if ip_address_parts[0] == 0:
+            return ipv4_address(software_allowed=False)
+
+        else:
+            return _construct_ip_address()
+
+    else:
+        return _construct_ip_address()
 
 
 def ipv6_address():
--- tests/test_iprandom.py
+++ tests/test_iprandom.py
@@ -1,16 +1,6 @@
 from iprandom import ipv4_address
 from iprandom import ipv6_address
 from string import hexdigits
-from ipaddress import IPv4Address
-
-
-# private network ip cidr
-# 10.0.0.0/8      =  10.0.0.0–10.255.255.255
-# 100.64.0.0/10   =  100.64.0.0–100.127.255.255
-# 172.16.0.0/12   =  172.16.0.0–172.31.255.255
-# 192.0.0.0/24    =  192.0.0.0–192.0.0.255
-# 192.168.0.0/16  =  192.168.0.0–192.168.255.255
-# 198.18.0.0/15   =  198.18.0.0–198.19.255.255
 
 
 def test_ipv4_address():
@@ -19,32 +9,144 @@ def test_ipv4_address():
     assert isinstance(ip_address, str)
     assert 7 <= len(ip_address) <= 15
 
-    split_ip_address = ip_address.split(".")
-    assert isinstance(split_ip_address, list)
-    assert len(split_ip_address) == 4
-    for i in split_ip_address:
+    _split_ip_address = ip_address.split(".")
+    assert isinstance(_split_ip_address, list)
+    assert len(_split_ip_address) == 4
+    for i in _split_ip_address:
         assert i.isdigit()
         assert 0 <= int(i) <= 255
 
+
+def test_ipv4_address_private_network_not_allowed():
+    """
+    private network ip cidr
+    10.0.0.0/8      =  10.0.0.0–10.255.255.255
+    100.64.0.0/10   =  100.64.0.0–100.127.255.255
+    172.16.0.0/12   =  172.16.0.0–172.31.255.255
+    192.0.0.0/24    =  192.0.0.0–192.0.0.255
+    192.168.0.0/16  =  192.168.0.0–192.168.255.255
+    198.18.0.0/15   =  198.18.0.0–198.19.255.255
+    """
     ip_address = ipv4_address(private_network_allowed=False)
     assert ip_address
     # 10.0.0.0/8
     assert not ip_address.startswith("10.")
     # 100.64.0.0/10
-    for i in range(64, 128):
-        assert not ip_address.startswith(f"100.{i}.")
+    assert not ip_address.startswith("100.64.")
+    assert not ip_address.startswith("100.65.")
+    assert not ip_address.startswith("100.66.")
+    assert not ip_address.startswith("100.67.")
+    assert not ip_address.startswith("100.68.")
+    assert not ip_address.startswith("100.69.")
+    assert not ip_address.startswith("100.70.")
+    assert not ip_address.startswith("100.71.")
+    assert not ip_address.startswith("100.72.")
+    assert not ip_address.startswith("100.73.")
+    assert not ip_address.startswith("100.74.")
+    assert not ip_address.startswith("100.75.")
+    assert not ip_address.startswith("100.76.")
+    assert not ip_address.startswith("100.77.")
+    assert not ip_address.startswith("100.78.")
+    assert not ip_address.startswith("100.79.")
+    assert not ip_address.startswith("100.80.")
+    assert not ip_address.startswith("100.81.")
+    assert not ip_address.startswith("100.82.")
+    assert not ip_address.startswith("100.83.")
+    assert not ip_address.startswith("100.84.")
+    assert not ip_address.startswith("100.85.")
+    assert not ip_address.startswith("100.86.")
+    assert not ip_address.startswith("100.87.")
+    assert not ip_address.startswith("100.88.")
+    assert not ip_address.startswith("100.89.")
+    assert not ip_address.startswith("100.90.")
+    assert not ip_address.startswith("100.91.")
+    assert not ip_address.startswith("100.92.")
+    assert not ip_address.startswith("100.93.")
+    assert not ip_address.startswith("100.94.")
+    assert not ip_address.startswith("100.95.")
+    assert not ip_address.startswith("100.96.")
+    assert not ip_address.startswith("100.97.")
+    assert not ip_address.startswith("100.98.")
+    assert not ip_address.startswith("100.99.")
+    assert not ip_address.startswith("100.100.")
+    assert not ip_address.startswith("100.101.")
+    assert not ip_address.startswith("100.102.")
+    assert not ip_address.startswith("100.103.")
+    assert not ip_address.startswith("100.104.")
+    assert not ip_address.startswith("100.105.")
+    assert not ip_address.startswith("100.106.")
+    assert not ip_address.startswith("100.107.")
+    assert not ip_address.startswith("100.108.")
+    assert not ip_address.startswith("100.109.")
+    assert not ip_address.startswith("100.110.")
+    assert not ip_address.startswith("100.111.")
+    assert not ip_address.startswith("100.112.")
+    assert not ip_address.startswith("100.113.")
+    assert not ip_address.startswith("100.114.")
+    assert not ip_address.startswith("100.115.")
+    assert not ip_address.startswith("100.116.")
+    assert not ip_address.startswith("100.117.")
+    assert not ip_address.startswith("100.118.")
+    assert not ip_address.startswith("100.119.")
+    assert not ip_address.startswith("100.120.")
+    assert not ip_address.startswith("100.121.")
+    assert not ip_address.startswith("100.122.")
+    assert not ip_address.startswith("100.123.")
+    assert not ip_address.startswith("100.124.")
+    assert not ip_address.startswith("100.125.")
+    assert not ip_address.startswith("100.126.")
+    assert not ip_address.startswith("100.127.")
     # 172.16.0.0/12
-    for i in range(16, 32):
-        assert not ip_address.startswith(f"172.{i}.")
+    assert not ip_address.startswith("172.16.")
+    assert not ip_address.startswith("172.17.")
+    assert not ip_address.startswith("172.18.")
+    assert not ip_address.startswith("172.19.")
+    assert not ip_address.startswith("172.20.")
+    assert not ip_address.startswith("172.21.")
+    assert not ip_address.startswith("172.22.")
+    assert not ip_address.startswith("172.23.")
+    assert not ip_address.startswith("172.24.")
+    assert not ip_address.startswith("172.25.")
+    assert not ip_address.startswith("172.26.")
+    assert not ip_address.startswith("172.27.")
+    assert not ip_address.startswith("172.28.")
+    assert not ip_address.startswith("172.29.")
+    assert not ip_address.startswith("172.30.")
+    assert not ip_address.startswith("172.31.")
     # 192.0.0.0/24
     assert not ip_address.startswith("192.0.0.")
     # 192.168.0.0/16
     assert not ip_address.startswith("192.168.")
+    # 198.18.0.0/15
+    assert not ip_address.startswith("198.18.")
+    assert not ip_address.startswith("198.19.")
+
+
+def test_ipv4_address_software_not_allowed():
+    """
+    software ip cidr
+    0.0.0.0/8
+    """
+    ip_address = ipv4_address(software_allowed=False)
+    assert ip_address
+    assert not ip_address.startswith("0.")
+
+
+def test_ipv4_address_host_not_allowed():
+    """
+    software ip cidr
+    0.0.0.0/8
+    """
+    ip_address = ipv4_address(host_allowed=False)
+    assert ip_address
+    assert not ip_address.startswith("127.")
 
 
 def test_ipv4_address_100000_times():
     for i in range(1000000):
         test_ipv4_address()
+        test_ipv4_address_private_network_not_allowed()
+        test_ipv4_address_software_not_allowed()
 
 
 def test_ipv6_address():
