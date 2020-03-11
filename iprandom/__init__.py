f107fc16b882f5b9fdb9bb6e926f39e4234f8a3d
jackwardell <jackwardell@me.com>
2020-03-11 07:19:47 +0000

    tdd: not allowing private networks part 1

--- iprandom/__init__.py
+++ iprandom/__init__.py
@@ -1,13 +1,41 @@
 import random
 import ipaddress
 
+# special address blocks
+# from wikipedia, https://en.wikipedia.org/wiki/Reserved_IP_addresses
 
-# reserved ips
+special_address_blocks = {
+    "0.0.0.0/8": ((0, 0, 0, 0), (0, 255, 255, 255)),
+    "10.0.0.0/8": ((10, 0, 0, 0), (10, 255, 255, 255))
+}
+
+# private network ip cidr
+# 10.0.0.0/8
+# 100.64.0.0/10
+# 172.16.0.0/12
+# 192.0.0.0/24
+# 192.168.0.0/16
+# 198.18.0.0/15
 
 
-def ipv4_address():
-    return ".".join([str(random.randint(0, 255)) for _ in range(4)])
+def ipv4_address(private_network_allowed: bool = True):
+    """
+    get a random ip address
+    """
+    ip_address = ".".join([str(random.randint(0, 255)) for _ in range(4)])
+
+    if private_network_allowed:
+        return ip_address
+
+    elif not private_network_allowed:
+        if ip_address.startswith("10."):
+            return ipv4_address(private_network_allowed=False)
+        else:
+            return ip_address
+
+
+
 
 
 def ipv6_address():
-    return ':'.join('{:04x}'.format(random.randint(0, 65535)) for _ in range(8))
+    return ":".join("{:04x}".format(random.randint(0, 65535)) for _ in range(8))
--- tests/test_iprandom.py
+++ tests/test_iprandom.py
@@ -1,5 +1,17 @@
 from iprandom import ipv4_address
 from iprandom import ipv6_address
+from string import hexdigits
+from ipaddress import IPv4Address
+
+
+# private network ip cidr
+# 10.0.0.0/8
+# 100.64.0.0/10
+# 172.16.0.0/12
+# 192.0.0.0/24
+# 192.168.0.0/16
+# 198.18.0.0/15
+
 
 def test_ipv4_address():
     ip_address = ipv4_address()
@@ -8,11 +20,36 @@ def test_ipv4_address():
     assert 7 <= len(ip_address) <= 15
 
     split_ip_address = ip_address.split(".")
-    assert len(split_ip_address) == 4
     assert isinstance(split_ip_address, list)
+    assert len(split_ip_address) == 4
     for i in split_ip_address:
         assert i.isdigit()
         assert 0 <= int(i) <= 255
 
+    ip_address = ipv4_address(private_network_allowed=False)
+    assert ip_address
+    # 10.0.0.0/8
+    assert not ip_address.startswith("10.")
+    # # 100.64.0.0/10
+    # for i in range(64, 128):
+    #     assert not ip_address.startswith(f"100.{i}")
 
-# def test_ipv6_address():
+
+def test_ipv4_address_1000_times():
+    for i in range(1000):
+        test_ipv4_address()
+
+
+def test_ipv6_address():
+    ip_address = ipv6_address()
+    assert ip_address
+    assert isinstance(ip_address, str)
+    assert len(ip_address) == 39
+
+    split_ip_address = ip_address.split(":")
+    assert isinstance(split_ip_address, list)
+    assert len(split_ip_address) == 8
+    for i in split_ip_address:
+        assert str(int(i, 16)).isdigit()
+        assert all([j in hexdigits for j in i])
+        assert 0 < int(i, 16) < int("ffff", 16)
