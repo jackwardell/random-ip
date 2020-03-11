85b189c07734e1357345d4e8a33c23a9f342fb26
jackwardell <jackwardell@me.com>
2020-03-11 06:27:38 +0000

    testing for ipv4

--- iprandom/__init__.py
+++ iprandom/__init__.py
@@ -1,2 +1,13 @@
+import random
+import ipaddress
+
+
+# reserved ips
+
+
 def ipv4_address():
-    pass
+    return ".".join([str(random.randint(0, 255)) for _ in range(4)])
+
+
+def ipv6_address():
+    return ':'.join('{:04x}'.format(random.randint(0, 65535)) for _ in range(8))
--- tests/test_iprandom.py
+++ tests/test_iprandom.py
@@ -1,4 +1,18 @@
 from iprandom import ipv4_address
+from iprandom import ipv6_address
 
 def test_ipv4_address():
     ip_address = ipv4_address()
+    assert ip_address
+    assert isinstance(ip_address, str)
+    assert 7 <= len(ip_address) <= 15
+
+    split_ip_address = ip_address.split(".")
+    assert len(split_ip_address) == 4
+    assert isinstance(split_ip_address, list)
+    for i in split_ip_address:
+        assert i.isdigit()
+        assert 0 <= int(i) <= 255
+
+
+# def test_ipv6_address():
