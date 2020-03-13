a921a1d49f0ecc98a8d7117555e91ab1bba1dfac
jackwardell <jackwardell@me.com>
2020-03-13 12:19:32 +0000

    refactoring & impliment much from ipaddress

--- iprandom/ip_v4.py
+++ iprandom/ip_v4.py
@@ -1,67 +1,60 @@
-def check_is_ipv4_address(ip_address):
-    assert isinstance(ip_address, str), "ip address must be a string"
-    assert (
-        7 <= len(ip_address) <= 15
-    ), "ip address must be between 7 and 15 characters inclusive"
-    ip_address_parts = ip_address.split(".")
-    for part in ip_address_parts:
-        assert part.isdigit(), "ip address parts must be digits"
-        assert (
-            0 <= int(part) <= 255
-        ), "ip address parts must be between 0 and 255 inclusive"
+# def check_is_ipv4_address(ip_address):
+#     assert isinstance(ip_address, str), "ip address must be a string"
+#     assert (
+#         7 <= len(ip_address) <= 15
+#     ), "ip address must be between 7 and 15 characters inclusive"
+#     ip_address_parts = ip_address.split(".")
+#     for part in ip_address_parts:
+#         assert part.isdigit(), "ip address parts must be digits"
+#         assert (
+#             0 <= int(part) <= 255
+#         ), "ip address parts must be between 0 and 255 inclusive"
+
+
+from ipaddress import IPv4Address
 
 
 class IPv4AddressRange:
     def __init__(self, start_ip, end_ip):
-        check_is_ipv4_address(start_ip)
         self.start_ip = start_ip
-        self.start_ip_parts = tuple(int(i) for i in start_ip.split("."))
-        (
-            self.start_ip_a,
-            self.start_ip_b,
-            self.start_ip_c,
-            self.start_ip_d,
-        ) = self.start_ip_parts
+        self._start_ip = IPv4Address(start_ip)
 
-        check_is_ipv4_address(end_ip)
         self.end_ip = end_ip
-        self.end_ip_parts = tuple(int(i) for i in end_ip.split("."))
-        (
-            self.end_ip_a,
-            self.end_ip_b,
-            self.end_ip_c,
-            self.end_ip_d,
-        ) = self.end_ip_parts
-        self.check_end_ip_is_greater_than_start_ip()
+        self._end_ip = IPv4Address(end_ip)
 
-        self.ip_a_range = range(self.start_ip_a, self.end_ip_a + 1)
+        assert self._start_ip < self._end_ip, "end ip address must be greater than start ip address"
 
-    def check_end_ip_is_greater_than_start_ip(self):
-        assert (
-            self.start_ip != self.end_ip
-        ), "start and end ips match - ip ranges should not match"
-        assert (
-            self.start_ip_a <= self.end_ip_a
-        ), "end ip must be greater than or equal to the start ip"
-        if self.start_ip_a == self.end_ip_a:
-            assert (
-                self.start_ip_b <= self.end_ip_b
-            ), "end ip must be greater than or equal to the start ip"
-            if self.start_ip_b == self.end_ip_b:
-                assert (
-                    self.start_ip_c <= self.end_ip_c
-                ), "end ip must be greater than or equal to the start ip"
-                if self.start_ip_c == self.start_ip_b:
-                    assert (
-                        self.start_ip_d < self.end_ip_d
-                    ), "end ip must be greater than or equal to the start ip"
+        self.ip_range = range(int(self._start_ip), int(self._end_ip) + 1)
 
-            # class IPv4Generator:
-            #     def __init__(self, start_ip="0.0.0.0", end_ip="255.255.255.255"):
-            #         check_is_ipv4_address(start_ip)
-            #         self.start_ip = start_ip
-            #         self.start_ip_parts = tuple(int(i) for i in start_ip.split("."))
-            #
-            #         check_is_ipv4_address(end_ip)
-            #         self.end_ip = end_ip
-            #         self.end_ip_parts = tuple(int(i) for i in end_ip.split("."))
+    def __contains__(self, ip_address):
+        _ip_address = IPv4Address(ip_address)
+        return int(_ip_address) in self.ip_range
+
+
+# def _check_end_ip_is_greater_than_start_ip(self):
+#     end_must_be_greater = "end ip must be greater than or equal to the start ip"
+#     assert self.start_ip != self.end_ip, (
+#         "start and end ips match - ip ranges should not match "
+#         "but rather the end ip should be greater than the start ip"
+#     )
+#     assert self.start_ip_parts[0] <= self.end_ip_parts[0], end_must_be_greater
+#     if self.start_ip_parts[0] == self.end_ip_parts[0]:
+#         assert self.start_ip_parts[1] <= self.end_ip_parts[1], end_must_be_greater
+#         if self.start_ip_parts[1] == self.end_ip_parts[1]:
+#             assert (
+#                 self.start_ip_parts[2] <= self.end_ip_parts[2]
+#             ), end_must_be_greater
+#             if self.start_ip_parts[2] == self.end_ip_parts[2]:
+#                 assert (
+#                     self.start_ip_parts[3] < self.end_ip_parts[3]
+#                 ), end_must_be_greater
+
+# class IPv4Generator:
+#     def __init__(self, start_ip="0.0.0.0", end_ip="255.255.255.255"):
+#         check_is_ipv4_address(start_ip)
+#         self.start_ip = start_ip
+#         self.start_ip_parts = tuple(int(i) for i in start_ip.split("."))
+#
+#         check_is_ipv4_address(end_ip)
+#         self.end_ip = end_ip
+#         self.end_ip_parts = tuple(int(i) for i in end_ip.split("."))
--- tests/test_ipv4.py
+++ tests/test_ipv4.py
@@ -1,46 +1,55 @@
 from iprandom.ip_v4 import IPv4AddressRange
+from ipaddress import IPv4Address
+from ipaddress import AddressValueError
 
 
 def test_ip_v4_address_range():
     from pytest import raises
 
     start_ip, end_ip = "0.0.0.0", "255.255.255.255"
+    start_ipv4, end_ipv4 = IPv4Address(start_ip), IPv4Address(end_ip)
     ip_address_range = IPv4AddressRange(start_ip, end_ip)
     assert ip_address_range
     assert ip_address_range.start_ip == start_ip
-    assert ip_address_range.start_ip_parts == tuple(int(i) for i in start_ip.split("."))
+    # assert ip_address_range.start_ip_parts == tuple(int(i) for i in start_ip.split("."))
     assert ip_address_range.end_ip == end_ip
-    assert ip_address_range.end_ip_parts == tuple(int(i) for i in end_ip.split("."))
-    assert ip_address_range.ip_a_range == range(
-        int(start_ip.split(".")[0]), int(end_ip.split(".")[0]) + 1
-    )
+    # assert ip_address_range.end_ip_parts == tuple(int(i) for i in end_ip.split("."))
+    assert ip_address_range.ip_range == range(int(start_ipv4), int(end_ipv4) + 1)
+
+    with raises(AddressValueError):
+        IPv4AddressRange("asgdjka", "asdkas")
+
+    with raises(AddressValueError):
+        IPv4AddressRange("234234.4.324.2", "23423.423.4.2")
+
+    with raises(AddressValueError):
+        IPv4AddressRange("0.0.0", "155.6.6")
 
     start_ip, end_ip = "255.255.255.255", "0.0.0.0"
-    with raises(
-        AssertionError, match="end ip must be greater than or equal to the start ip"
-    ):
+    with raises(AssertionError):
         ip_address_range = IPv4AddressRange(start_ip, end_ip)
 
     start_ip, end_ip = "10.10.0.0", "10.0.0.0"
-    with raises(
-        AssertionError, match="end ip must be greater than or equal to the start ip"
-    ):
+    with raises(AssertionError):
         ip_address_range = IPv4AddressRange(start_ip, end_ip)
 
     start_ip, end_ip = "10.10.10.0", "10.10.0.0"
-    with raises(
-        AssertionError, match="end ip must be greater than or equal to the start ip"
-    ):
+    with raises(AssertionError):
         ip_address_range = IPv4AddressRange(start_ip, end_ip)
 
     start_ip, end_ip = "10.10.10.10", "10.10.10.0"
-    with raises(
-        AssertionError, match="end ip must be greater than or equal to the start ip"
-    ):
+    with raises(AssertionError):
         ip_address_range = IPv4AddressRange(start_ip, end_ip)
 
     start_ip, end_ip = "0.0.0.0", "0.0.0.0"
-    with raises(
-        AssertionError, match="start and end ips match - ip ranges should not match"
-    ):
+    with raises(AssertionError):
         ip_address_range = IPv4AddressRange(start_ip, end_ip)
+
+    ip_address_range = IPv4AddressRange("0.0.0.0", "255.255.255.255")
+    assert "1.2.3.4" in ip_address_range
+
+    ip_address_range = IPv4AddressRange("127.0.0.0", "255.255.255.0")
+    assert "1.2.3.4" not in ip_address_range
+    assert "127.0.0.0" in ip_address_range
+    assert "255.255.255.0" in ip_address_range
+    assert "255.255.255.1" not in ip_address_range
